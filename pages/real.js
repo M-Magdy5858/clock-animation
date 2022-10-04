d2cca7af243944f4228e625d6ed6cfd11d4e54c6
M-Magdy5858 <m.magdy5858@gmail.com>
2022-10-04 21:50:27 +0200

    removed sound

--- pages/real.js
+++ pages/real.js
@@ -1,19 +1,14 @@
 var d;
-var sound = new Audio('../assets/pop.mp3');
-var time = setInterval(function () {
-	sound.play();
+function updateTime() {
 	d = new Date();
 	hours = d.getHours();
 	min = d.getMinutes();
 	sec = d.getSeconds();
 
-	document.getElementById('sec').style.transform = `rotate(${
-		-90 + sec * 6
-	}deg )`;
-	document.getElementById('min').style.transform = `rotate(${
-		-90 + min * 6
-	}deg)`;
-	document.getElementById('hour').style.transform = `rotate(${
-		-90 + (hours + min / 60) * 30
-	}deg)`;
-}, 1000);
+	document.getElementById('sec').style.transform = `rotate(${-90 + sec * 6}deg )`;
+	document.getElementById('min').style.transform = `rotate(${-90 + min * 6}deg)`;
+	document.getElementById('hour').style.transform = `rotate(${-90 + (hours + min / 60) * 30}deg)`;
+}
+
+updateTime();
+var time = setInterval(updateTime, 1000);
